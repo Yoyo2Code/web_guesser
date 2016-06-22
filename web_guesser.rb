1c7817dd7ea2965368810de44fbee5bcb609a864
Yoseph Anderson <yrganderson@gmail.com>
2016-06-21 19:49:03 -0600

    finished

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,7 +1,77 @@
 require 'sinatra'
 require 'sinatra/reloader'
-number = rand(100)
+
+@@secret_number = rand(100)
+@@guess_count   = 5
+
+  def check_guess(guess)
+    guess = guess.to_i
+    unless guess == @@secret_number
+      guess > @@secret_number ? high_guess(guess) : low_guess(guess)
+    else
+      congrats_message
+    end
+  end
+
+  def high_guess(guess)
+    guess > (@@secret_number + 5) ? "Way Too High!" : "Too High!"
+  end
+
+  def low_guess(guess)
+    guess < (@@secret_number - 5) ? "Way Too Low!" : "Too Low!"
+  end
+
+  def congrats_message
+    "You got it right!"
+  end
+
+  def show_secret?(message)
+    "The SECRET NUMBER is #{@@secret_number}" if win?(message) || cheat_mode?
+  end
+
+  def cheat_mode?
+    params["cheat"] == 'true'
+  end
+
+  def win?(message)
+    message == congrats_message
+  end
+
+  def color_match(message)
+    if win?(message)
+      "#3EC5A0"
+    else
+      message.include?("Way") ? "#E63838" : "#FF6666"
+    end
+  end
+
+  def check_game_status(message)
+    @@guess_count -= 1
+    if @@guess_count == 0
+      start_new_game
+      message = "You Failed! A new SECRET NUMBER has been generated."
+    elsif win?(message)
+      start_new_game
+      message
+    else
+      message
+    end
+  end
+
+  def start_start_new_game
+    @@secret_number = rand(100)
+    @@guess_count   = 5
+  end
 
 get '/' do
-  erb :index, :locals => {:number => number}
+  guess      = params["guess"]
+  submitted  = check_guess(guess)
+  background = color_match(submitted)
+  secret     = show_secret?(submitted)
+  message    = check_game_status(submitted)
+  erb :index, locals: {
+                        message: message,
+                        secret: secret,
+                        background: background
+                      }
 end
